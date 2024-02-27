a7433655cd63882fa89e11c497e2f4a5cc3af08f
NikhilCherupally <nikhilnikkicherupally@gmail.com>
2024-02-27 11:20:44 +0530

    tl - task 2

---
+++ proj1/src/main/java/nit/it/proj1/AppTest.java
@@ -0,0 +1,12 @@
+package nit.it.proj1;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	
+
+	}
+
+}
---
+++ proj1/src/test/java/nit/it/proj1/Operators.java
@@ -0,0 +1,14 @@
+package nit.it.proj1;
+
+public class Operators {
+	
+
+		public int sum(int x, int y) {
+			return x+y;
+		}
+		public int sub(int x, int y) {
+			return x-y;
+		}
+	}
+
+
